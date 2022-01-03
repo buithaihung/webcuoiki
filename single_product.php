e0b659ec5f7f1ee220511c474c5ccc38c43d54c1
buithaihung <buithaihung6934@gmail.com>
2022-01-03 20:48:13 +0700

    fix

--- single_product.php
+++ single_product.php
@@ -100,149 +100,38 @@ if(isset($_GET['product_id'])){
 
 
 <!--Realated products-->
-<section id="related-products" class="my-5 pb-5">
+<section id="featured" class="my-5 pb-5">
     <div class="container text-center mt-5 py-5">
-        <h3>Related Products</h3>
+        <h3>Other Products</h3>
         <hr class="mx-auto">
+        <p>Here you can check out our other products</p>
     </div>
     <div class="row mx-auto container-fluid">
-        <div class="product text-center col-lg-3 col-md-4 col-sm-12">
-            <img class="img-fluid mb-3" src="assets/imgs/featured1.jpeg" />
-            <div class="star">
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
+
+        <?php include('server/get_featured_products.php'); ?>
+
+
+        <?php while ($row = $featured_products->fetch_assoc()) { ?>
+
+            <div class="product text-center col-lg-3 col-md-4 col-sm-12">
+                <a href="<?php echo "single_product.php?product_id=" . $row['product_id']; ?>"> <img class="img-fluid mb-3" src="assets/imgs/<?php echo $row['product_image']; ?>" /></a>
+                <div class="star">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <h5 class="p-name"><?php echo $row['product_name']; ?></h5>
+                <h4 class="p-price">$ <?php echo $row['product_price']; ?></h4>
+                <a href="<?php echo "single_product.php?product_id=" . $row['product_id']; ?>"><button class="buy-btn">Buy
+                        Now</button></a>
             </div>
-            <h5 class="p-name">Sports Shoes</h5>
-            <h4 class="p-price">$199.8</h4>
-            <button class="buy-btn">Buy Now</button>
-        </div>
-        <div class="product text-center col-lg-3 col-md-4 col-sm-12">
-            <img class="img-fluid mb-3" src="assets/imgs/featured2.jpeg" />
-            <div class="star">
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-            </div>
-            <h5 class="p-name">Sports Shoes</h5>
-            <h4 class="p-price">$199.8</h4>
-            <button class="buy-btn">Buy Now</button>
-        </div>
-        <div class="product text-center col-lg-3 col-md-4 col-sm-12">
-            <img class="img-fluid mb-3" src="assets/imgs/featured3.jpeg" />
-            <div class="star">
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-            </div>
-            <h5 class="p-name">Sports Shoes</h5>
-            <h4 class="p-price">$199.8</h4>
-            <button class="buy-btn">Buy Now</button>
-        </div>
-        <div class="product text-center col-lg-3 col-md-4 col-sm-12">
-            <img class="img-fluid mb-3" src="assets/imgs/featured4.jpeg" />
-            <div class="star">
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-            </div>
-            <h5 class="p-name">Sports Shoes</h5>
-            <h4 class="p-price">$199.8</h4>
-            <button class="buy-btn">Buy Now</button>
-        </div>
+
+
+        <?php } ?>
     </div>
 </section>
-
-
-
-
-
-
-
-
-
-<!--Footer-->
-<footer class="mt-5 py-5">
-    <div class="row container mx-auto pt-5">
-        <div class="footer-one col-lg-3 col-md-6 col-sm-12">
-            <img class="logo" src="assets/imgs/logo.jpeg" />
-            <p class="pt-3">We provide the best products for the most affordable prices</p>
-        </div>
-        <div class="footer-one col-lg-3 col-md-6 col-sm-12">
-            <h5 class="pb-2">Featured</h5>
-            <ul class="text-uppercase">
-                <li><a href="#">men</a></li>
-                <li><a href="#">women</a></li>
-                <li><a href="#">boys</a></li>
-                <li><a href="#">girls</a></li>
-                <li><a href="#">new arrivals</a></li>
-                <li><a href="#">clothes</a></li>
-            </ul>
-        </div>
-
-        <div class="footer-one col-lg-3 col-md-6 col-sm-12">
-            <h5 class="pb-2">Contact Us</h5>
-            <div>
-                <h6 class="text-uppercase">Address</h6>
-                <p>1234 Street Name, City</p>
-            </div>
-            <div>
-                <h6 class="text-uppercase">Phone</h6>
-                <p>123 456 7890</p>
-            </div>
-            <div>
-                <h6 class="text-uppercase">Email</h6>
-                <p>info@email.com</p>
-            </div>
-        </div>
-        <div class="footer-one col-lg-3 col-md-6 col-sm-12">
-            <h5 class="pb-2">Instagram</h5>
-            <div class="row">
-                <img src="assets/imgs/featured1.jpeg" class="img-fluid w-25 h-100 m-2" />
-                <img src="assets/imgs/featured2.jpeg" class="img-fluid w-25 h-100 m-2" />
-                <img src="assets/imgs/featured3.jpeg" class="img-fluid w-25 h-100 m-2" />
-                <img src="assets/imgs/featured4.jpeg" class="img-fluid w-25 h-100 m-2" />
-                <img src="assets/imgs/clothes1.jpeg" class="img-fluid w-25 h-100 m-2" />
-            </div>
-        </div>
-    </div>
-
-
-
-    <div class="copyright mt-5">
-        <div class="row container mx-auto">
-            <div class="col-lg-3 col-md-5 col-sm-12 mb-4">
-                <img src="assets/imgs/payment.jpeg" />
-            </div>
-            <div class="col-lg-3 col-md-5 col-sm-12 mb-4 text-nowrap mb-2">
-                <p>eCommerce @ 2025 All Right Reserved</p>
-            </div>
-            <div class="col-lg-3 col-md-5 col-sm-12 mb-4">
-                <a href="#"><i class="fab fa-facebook"></i></a>
-                <a href="#"><i class="fab fa-instagram"></i></a>
-                <a href="#"><i class="fab fa-twitter"></i></a>
-            </div>
-        </div>
-    </div>
-
-</footer>
-
-
-
-
-
-
-
-
-
 <script>
 var mainImg = document.getElementById("mainImg");
 var smallImg = document.getElementsByClassName("small-img");
